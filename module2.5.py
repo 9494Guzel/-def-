df32b4024a8792573c16c1d399058493f6506d2a
9494Guzel <guzel.galiulina.94@inbox.ru>
2024-07-23 00:13:26 +0300

    Add files via upload

---
+++ module2.5.py
@@ -0,0 +1,20 @@
+def get_matrix(n, m, value):
+    matrix()
+
+
+if value <= 0:
+    return matrix
+for i in range(n):
+    matrix.append([])
+for i in range(m):
+    matrix[-1].append(value)
+    return matrix()
+def get_matrix(n:2, m: 4, value: 6):
+    print(n ,m , value)
+    test([1, 2, 3])
+
+    def get_matrix(n: 5, m: 3, value: 8):
+        print(n, m, value)
+        test([1, 2, 3])
+
+
